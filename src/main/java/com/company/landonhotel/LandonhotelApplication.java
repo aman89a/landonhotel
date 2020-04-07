f7bd7da18175a5b9f23f0065c0b3a8c9598281ec
Аман Абдулаев <abdulaev25@gmail.com>
2020-04-07 15:19:03 +0600

    initial commit

---
+++ src/main/java/com/company/landonhotel/LandonhotelApplication.java
@@ -0,0 +1,13 @@
+package com.company.landonhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandonhotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandonhotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/landonhotel/LandonhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.landonhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandonhotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
