16b79d84a82fe03a7e655c11b0462587bec1a85e
Аман Абдулаев <abdulaev25@gmail.com>
2020-04-08 19:55:56 +0600

    add entity and repository.

---
+++ src/main/java/com/company/landonhotel/data/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.company.landonhotel.data.repository;
+
+
+import com.company.landonhotel.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
+    Room findByNumber(String number);
+}
